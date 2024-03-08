a47dcb562d16fa0a16bd1ede125b5a29bf293121
Alan Lu <alu36@ucsc.edu>
2024-03-08 01:28:17 -0800

    updated map, respawn and checkpoint

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -11,18 +11,12 @@ class Play extends Phaser.Scene {
         //game status
         this.gameStart = false
         this.gameOver = false
+        this.death = false
+        this.check1 = false
 
         //add background
         this.sky = this.add.tileSprite(0, 0, 950, 800, 'sky').setOrigin(0, 0)
         this.sky.fixedToCamera = true
-
-        //player specifications
-        this.player = this.physics.add.sprite(gameWidth/2, gameHeight/2, 'playerIdle', 0).setScale(0.7, 0.7)
-        this.player.play('idle')
-        this.player.setGravityY(1400)
-        this.player.body.setSize(60, 123)
-        this.player.body.setOffset(30, 22)
-        this.player.setCollideWorldBounds(true)
 
         //add enemy
         this.boss = this.physics.add.sprite(gameWidth/4, gameHeight/2 + 80, 'boss')
@@ -39,6 +33,19 @@ class Play extends Phaser.Scene {
         const tileset = map.addTilesetImage('tileset', 'tilesetImage')
         const bgLayer = map.createLayer('Background', tileset, 0, 0)
 
+        //spawn and checkpoints
+        this.spawn = map.findObject('Points', (obj) => obj.name === 'spawn1')
+
+        this.point1 = map.findObject('Points', (obj) => obj.name === 'spawn2')
+
+        //player specifications
+        this.player = this.physics.add.sprite(this.spawn.x, this.spawn.y, 'playerIdle', 0).setScale(0.7, 0.7)
+        this.player.play('idle')
+        this.player.setGravityY(1400)
+        this.player.body.setSize(60, 123)
+        this.player.body.setOffset(30, 22)
+        this.player.setCollideWorldBounds(true)     
+
         //collision with map
         bgLayer.setCollisionByProperty({collides: true})
         this.physics.add.collider(this.player, bgLayer)
@@ -53,9 +60,17 @@ class Play extends Phaser.Scene {
         //collision with enemy
         this.physics.add.collider(this.player, this.boss)
 
+        //test key
+        keyC = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.C)
+
     }
 
     update() {
+        //test key
+        if (Phaser.Input.Keyboard.JustDown(keyC)) {
+            this.death = true
+        }
+
         //checks keys for player movement
         if(cursors.left.isDown) {
             this.player.body.setVelocityX(-200)
@@ -65,6 +80,7 @@ class Play extends Phaser.Scene {
             this.player.body.setVelocityX(200)
             this.player.resetFlip()
             this.player.anims.play('run', true)
+            this.sky.tilePositionX += 2.9 
         } else {
             this.player.body.setVelocityX(0)
             this.player.anims.play('idle', true)
@@ -79,8 +95,29 @@ class Play extends Phaser.Scene {
             this.player.body.setVelocityY(-600)
         }
         
+        //sky background config
         this.sky.setPosition.X = this.game.fixedToCamera
         this.sky.setScrollFactor(0)
-        this.sky.tilePositionX += 4         
+        this.sky.tilePositionX += 1  
+
+        //checkpoint system
+        this.physics.add.overlap(this.player, this.point1, () => {
+            this.check1 = true
+        })
+
+        //call respawn
+        if (this.death == true) {
+            if (this.check1 == true) {
+                this.player.setX(this.point1.x)
+                this.player.setY(this.point1.y)
+                this.death = false
+            }
+            else {
+                this.player.setX(this.spawn.x)
+                this.player.setY(this.spawn.y)
+                this.death = false
+            }
+        }
     }
+    
 }
